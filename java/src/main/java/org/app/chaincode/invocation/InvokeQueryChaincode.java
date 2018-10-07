e508f571840f1d3b010cb774278c1200704c65b4
PAUL <yaolj@ebikj.com>
2018-10-07 19:06:52 +0800

    IP配修正

--- java/src/main/java/org/app/chaincode/invocation/InvokeQueryChaincode.java
+++ java/src/main/java/org/app/chaincode/invocation/InvokeQueryChaincode.java
@@ -9,14 +9,8 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
  *  See the License for the specific language governing permissions and 
  *  limitations under the License.
- */ 
+ */
 package org.app.chaincode.invocation;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.app.client.CAClient;
 import org.app.client.ChannelClient;
@@ -24,86 +18,85 @@ import org.app.client.FabricClient;
 import org.app.config.Config;
 import org.app.user.UserContext;
 import org.app.util.Util;
-import org.hyperledger.fabric.sdk.ChaincodeID;
-import org.hyperledger.fabric.sdk.Channel;
-import org.hyperledger.fabric.sdk.EventHub;
-import org.hyperledger.fabric.sdk.Orderer;
-import org.hyperledger.fabric.sdk.Peer;
-import org.hyperledger.fabric.sdk.ProposalResponse;
-import org.hyperledger.fabric.sdk.TransactionProposalRequest;
+import org.hyperledger.fabric.sdk.*;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.app.config.Config.HOST_IP;
 
 /**
- * 
  * @author Balaji Kadambi
- *
  */
 
 public class InvokeQueryChaincode {
 
-	private static final byte[] EXPECTED_EVENT_DATA = "!".getBytes(UTF_8);
-	private static final String EXPECTED_EVENT_NAME = "event";
+    private static final byte[] EXPECTED_EVENT_DATA = "!".getBytes(UTF_8);
+    private static final String EXPECTED_EVENT_NAME = "event";
 
-	public static void main(String args[]) {
-		try {
+    public static void main(String args[]) {
+        try {
             Util.cleanUp();
-			String caUrl = Config.CA_ORG1_URL;
-			CAClient caClient = new CAClient(caUrl, null);
-			// Enroll Admin to Org1MSP
-			UserContext adminUserContext = new UserContext();
-			adminUserContext.setName(Config.ADMIN);
-			adminUserContext.setAffiliation(Config.ORG1);
-			adminUserContext.setMspId(Config.ORG1_MSP);
-			caClient.setAdminUserContext(adminUserContext);
-			adminUserContext = caClient.enrollAdminUser(Config.ADMIN, Config.ADMIN_PASSWORD);
-			
-			FabricClient fabClient = new FabricClient(adminUserContext);
-			
-			ChannelClient channelClient = fabClient.createChannelClient(Config.CHANNEL_NAME);
-			Channel channel = channelClient.getChannel();
-			Peer peer = fabClient.getInstance().newPeer(Config.ORG1_PEER_0, Config.ORG1_PEER_0_URL);
-			EventHub eventHub = fabClient.getInstance().newEventHub("eventhub01", "grpc://localhost:7053");
-			Orderer orderer = fabClient.getInstance().newOrderer(Config.ORDERER_NAME, Config.ORDERER_URL);
-			channel.addPeer(peer);
-			channel.addEventHub(eventHub);
-			channel.addOrderer(orderer);
-			channel.initialize();
+            String caUrl = Config.CA_ORG1_URL;
+            CAClient caClient = new CAClient(caUrl, null);
+            // Enroll Admin to Org1MSP
+            UserContext adminUserContext = new UserContext();
+            adminUserContext.setName(Config.ADMIN);
+            adminUserContext.setAffiliation(Config.ORG1);
+            adminUserContext.setMspId(Config.ORG1_MSP);
+            caClient.setAdminUserContext(adminUserContext);
+            adminUserContext = caClient.enrollAdminUser(Config.ADMIN, Config.ADMIN_PASSWORD);
 
-			TransactionProposalRequest request = fabClient.getInstance().newTransactionProposalRequest();
-			ChaincodeID ccid = ChaincodeID.newBuilder().setName(Config.CHAINCODE_1_NAME).build();
-			request.setChaincodeID(ccid);
-			request.setFcn("createCar");
-			String[] arguments = { "CAR1", "Chevy", "Volt", "Red", "Nick" };
-			request.setArgs(arguments);
-			request.setProposalWaitTime(1000);
+            FabricClient fabClient = new FabricClient(adminUserContext);
 
-			Map<String, byte[]> tm2 = new HashMap<>();
-			tm2.put("HyperLedgerFabric", "TransactionProposalRequest:JavaSDK".getBytes(UTF_8)); // Just some extra junk
-																								// in transient map
-			tm2.put("method", "TransactionProposalRequest".getBytes(UTF_8)); // ditto
-			tm2.put("result", ":)".getBytes(UTF_8)); // This should be returned see chaincode why.
-			tm2.put(EXPECTED_EVENT_NAME, EXPECTED_EVENT_DATA); // This should trigger an event see chaincode why.
-			request.setTransientMap(tm2);
-			Collection<ProposalResponse> responses = channelClient.sendTransactionProposal(request);
-			
-			Thread.sleep(10000);
-			
-			Collection<ProposalResponse>  responsesQuery = channelClient.queryByChainCode("fabcar", "queryAllCars", null);
-			for (ProposalResponse pres : responsesQuery) {
-				String stringResponse = new String(pres.getChaincodeActionResponsePayload());
-				System.out.println(stringResponse);
-			}
+            ChannelClient channelClient = fabClient.createChannelClient(Config.CHANNEL_NAME);
+            Channel channel = channelClient.getChannel();
+            Peer peer = fabClient.getInstance().newPeer(Config.ORG1_PEER_0, Config.ORG1_PEER_0_URL);
+            EventHub eventHub = fabClient.getInstance().newEventHub("eventhub01", "grpc://" + HOST_IP + ":7053");
+            Orderer orderer = fabClient.getInstance().newOrderer(Config.ORDERER_NAME, Config.ORDERER_URL);
+            channel.addPeer(peer);
+            channel.addEventHub(eventHub);
+            channel.addOrderer(orderer);
+            channel.initialize();
 
-			Thread.sleep(10000);
-			String[] args1 = {"CAR1"};
-			Collection<ProposalResponse>  responses1Query = channelClient.queryByChainCode("fabcar", "queryCar", args1);
-			for (ProposalResponse pres : responses1Query) {
-				String stringResponse = new String(pres.getChaincodeActionResponsePayload());
-				System.out.println(stringResponse);
-			}		
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            TransactionProposalRequest request = fabClient.getInstance().newTransactionProposalRequest();
+            ChaincodeID ccid = ChaincodeID.newBuilder().setName(Config.CHAINCODE_1_NAME).build();
+            request.setChaincodeID(ccid);
+            request.setFcn("createCar");
+            String[] arguments = {"CAR1", "Chevy", "Volt", "Red", "Nick"};
+            request.setArgs(arguments);
+            request.setProposalWaitTime(1000);
+
+            Map<String, byte[]> tm2 = new HashMap<>();
+            tm2.put("HyperLedgerFabric", "TransactionProposalRequest:JavaSDK".getBytes(UTF_8)); // Just some extra junk
+            // in transient map
+            tm2.put("method", "TransactionProposalRequest".getBytes(UTF_8)); // ditto
+            tm2.put("result", ":)".getBytes(UTF_8)); // This should be returned see chaincode why.
+            tm2.put(EXPECTED_EVENT_NAME, EXPECTED_EVENT_DATA); // This should trigger an event see chaincode why.
+            request.setTransientMap(tm2);
+            Collection<ProposalResponse> responses = channelClient.sendTransactionProposal(request);
+
+            Thread.sleep(10000);
+
+            Collection<ProposalResponse> responsesQuery = channelClient.queryByChainCode("fabcar", "queryAllCars", null);
+            for (ProposalResponse pres : responsesQuery) {
+                String stringResponse = new String(pres.getChaincodeActionResponsePayload());
+                System.out.println(stringResponse);
+            }
+
+            Thread.sleep(10000);
+            String[] args1 = {"CAR1"};
+            Collection<ProposalResponse> responses1Query = channelClient.queryByChainCode("fabcar", "queryCar", args1);
+            for (ProposalResponse pres : responses1Query) {
+                String stringResponse = new String(pres.getChaincodeActionResponsePayload());
+                System.out.println(stringResponse);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- java/src/main/java/org/app/chaincode/invocation/QueryChaincode.java
+++ java/src/main/java/org/app/chaincode/invocation/QueryChaincode.java
@@ -9,14 +9,8 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
  *  See the License for the specific language governing permissions and 
  *  limitations under the License.
- */ 
+ */
 package org.app.chaincode.invocation;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-
-import java.util.Collection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.app.client.CAClient;
 import org.app.client.ChannelClient;
@@ -24,68 +18,69 @@ import org.app.client.FabricClient;
 import org.app.config.Config;
 import org.app.user.UserContext;
 import org.app.util.Util;
-import org.hyperledger.fabric.sdk.Channel;
-import org.hyperledger.fabric.sdk.EventHub;
-import org.hyperledger.fabric.sdk.Orderer;
-import org.hyperledger.fabric.sdk.Peer;
-import org.hyperledger.fabric.sdk.ProposalResponse;
+import org.hyperledger.fabric.sdk.*;
+
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.app.config.Config.HOST_IP;
 
 /**
- * 
  * @author Balaji Kadambi
- *
  */
 
 public class QueryChaincode {
 
-	private static final byte[] EXPECTED_EVENT_DATA = "!".getBytes(UTF_8);
-	private static final String EXPECTED_EVENT_NAME = "event";
+    private static final byte[] EXPECTED_EVENT_DATA = "!".getBytes(UTF_8);
+    private static final String EXPECTED_EVENT_NAME = "event";
 
-	public static void main(String args[]) {
-		try {
+    public static void main(String args[]) {
+        try {
             Util.cleanUp();
-			String caUrl = Config.CA_ORG1_URL;
-			CAClient caClient = new CAClient(caUrl, null);
-			// Enroll Admin to Org1MSP
-			UserContext adminUserContext = new UserContext();
-			adminUserContext.setName(Config.ADMIN);
-			adminUserContext.setAffiliation(Config.ORG1);
-			adminUserContext.setMspId(Config.ORG1_MSP);
-			caClient.setAdminUserContext(adminUserContext);
-			adminUserContext = caClient.enrollAdminUser(Config.ADMIN, Config.ADMIN_PASSWORD);
-			
-			FabricClient fabClient = new FabricClient(adminUserContext);
-			
-			ChannelClient channelClient = fabClient.createChannelClient(Config.CHANNEL_NAME);
-			Channel channel = channelClient.getChannel();
-			Peer peer = fabClient.getInstance().newPeer(Config.ORG1_PEER_0, Config.ORG1_PEER_0_URL);
-			EventHub eventHub = fabClient.getInstance().newEventHub("eventhub01", "grpc://localhost:7053");
-			Orderer orderer = fabClient.getInstance().newOrderer(Config.ORDERER_NAME, Config.ORDERER_URL);
-			channel.addPeer(peer);
-			channel.addEventHub(eventHub);
-			channel.addOrderer(orderer);
-			channel.initialize();
+            String caUrl = Config.CA_ORG1_URL;
+            CAClient caClient = new CAClient(caUrl, null);
+            // Enroll Admin to Org1MSP
+            UserContext adminUserContext = new UserContext();
+            adminUserContext.setName(Config.ADMIN);
+            adminUserContext.setAffiliation(Config.ORG1);
+            adminUserContext.setMspId(Config.ORG1_MSP);
+            caClient.setAdminUserContext(adminUserContext);
+            adminUserContext = caClient.enrollAdminUser(Config.ADMIN, Config.ADMIN_PASSWORD);
 
-			Logger.getLogger(QueryChaincode.class.getName()).log(Level.INFO, "Querying for all cars ...");
-			Collection<ProposalResponse>  responsesQuery = channelClient.queryByChainCode("fabcar", "queryAllCars", null);
-			for (ProposalResponse pres : responsesQuery) {
-				String stringResponse = new String(pres.getChaincodeActionResponsePayload());
-				Logger.getLogger(QueryChaincode.class.getName()).log(Level.INFO, stringResponse);
-			}
+            FabricClient fabClient = new FabricClient(adminUserContext);
 
-			Thread.sleep(10000);
-			String[] args1 = {"CAR1"};
-			Logger.getLogger(QueryChaincode.class.getName()).log(Level.INFO, "Querying for a car - " + args1[0]);
-			
-			Collection<ProposalResponse>  responses1Query = channelClient.queryByChainCode("fabcar", "queryCar", args1);
-			for (ProposalResponse pres : responses1Query) {
-				String stringResponse = new String(pres.getChaincodeActionResponsePayload());
-				Logger.getLogger(QueryChaincode.class.getName()).log(Level.INFO, stringResponse);
-			}		
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            ChannelClient channelClient = fabClient.createChannelClient(Config.CHANNEL_NAME);
+            Channel channel = channelClient.getChannel();
+            Peer peer = fabClient.getInstance().newPeer(Config.ORG1_PEER_0, Config.ORG1_PEER_0_URL);
+            EventHub eventHub = fabClient.getInstance().newEventHub("eventhub01", "grpc://" + HOST_IP + ":7053");
+            Orderer orderer = fabClient.getInstance().newOrderer(Config.ORDERER_NAME, Config.ORDERER_URL);
+            channel.addPeer(peer);
+            channel.addEventHub(eventHub);
+            channel.addOrderer(orderer);
+            channel.initialize();
+
+            Logger.getLogger(QueryChaincode.class.getName()).log(Level.INFO, "Querying for all cars ...");
+            Collection<ProposalResponse> responsesQuery = channelClient.queryByChainCode("fabcar", "queryAllCars", null);
+            for (ProposalResponse pres : responsesQuery) {
+                String stringResponse = new String(pres.getChaincodeActionResponsePayload());
+                Logger.getLogger(QueryChaincode.class.getName()).log(Level.INFO, stringResponse);
+            }
+
+            Thread.sleep(10000);
+            String[] args1 = {"CAR1"};
+            Logger.getLogger(QueryChaincode.class.getName()).log(Level.INFO, "Querying for a car - " + args1[0]);
+
+            Collection<ProposalResponse> responses1Query = channelClient.queryByChainCode("fabcar", "queryCar", args1);
+            for (ProposalResponse pres : responses1Query) {
+                String stringResponse = new String(pres.getChaincodeActionResponsePayload());
+                Logger.getLogger(QueryChaincode.class.getName()).log(Level.INFO, stringResponse);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
